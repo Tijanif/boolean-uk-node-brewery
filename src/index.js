a684e34955bcf8015969112d56bea93e45663a23
Tijani Fulani <Fulani07@gmail.com>
2021-07-21 16:08:40 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,85 @@
+// Dependencies
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+/* SETUP MIDDLEWARE */
+
+app.use(morgan('dev'));
+
+/* SETUP ROUTES */
+
+const breweries = [
+  {
+    id: 9094,
+    obdb_id: 'bnaf-llc-austin',
+    name: 'Bnaf, LLC',
+    brewery_type: 'planning',
+    street: null,
+    address_2: null,
+    address_3: null,
+    city: 'Austin',
+    state: 'Texas',
+    county_province: null,
+    postal_code: '78727-7602',
+    country: 'United States',
+    longitude: null,
+    latitude: null,
+    phone: null,
+    website_url: null,
+    updated_at: '2018-07-24T00:00:00.000Z',
+    created_at: '2018-07-24T00:00:00.000Z',
+  },
+  {
+    id: 9180,
+    obdb_id: 'boulder-beer-co-boulder',
+    name: 'Boulder Beer Co',
+    brewery_type: 'regional',
+    street: '2880 Wilderness Pl',
+    address_2: null,
+    address_3: null,
+    city: 'Boulder',
+    state: 'Colorado',
+    county_province: null,
+    postal_code: '80301-5401',
+    country: 'United States',
+    longitude: '-105.2480158',
+    latitude: '40.026439',
+    phone: null,
+    website_url: null,
+    updated_at: '2018-08-24T00:00:00.000Z',
+    created_at: '2018-07-24T00:00:00.000Z',
+  },
+  {
+    id: 9754,
+    obdb_id: 'clermont-brewing-company-clermont',
+    name: 'Clermont Brewing Company',
+    brewery_type: 'planning',
+    street: null,
+    address_2: null,
+    address_3: null,
+    city: 'Clermont',
+    state: 'Florida',
+    county_province: null,
+    postal_code: '34711-2108',
+    country: 'United States',
+    longitude: null,
+    latitude: null,
+    phone: null,
+    website_url: null,
+    updated_at: '2018-08-11T00:00:00.000Z',
+    created_at: '2018-07-24T00:00:00.000Z',
+  },
+];
+app.get('/breweries', (req, res) => {
+  res.json({ breweries });
+});
+
+/* START SERVER */
+
+const port = 3030;
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}/`);
+});
